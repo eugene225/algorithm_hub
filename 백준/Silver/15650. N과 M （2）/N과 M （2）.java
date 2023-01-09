b714086219f26a5bc9ef41bcf767e10aec161e2e
eugene <81062608+eugene225@users.noreply.github.com>
2023-01-09 21:58:53 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());  // 1~N 까지의 수
+        int M = Integer.parseInt(st.nextToken());  // 중복없이 M개의 숫자를 뽑아서 나열
+
+        arr = new int[M];
+        visited = new boolean[N];
+        dfs(N, M, 0);
+        System.out.print(sb);
+
+    }
+
+    static void dfs(int N, int M, int depth){
+        if(depth == M){
+            for(int val : arr){
+                sb.append(val).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            if(!visited[i] && (depth==0 || arr[depth-1] < i+1)){
+                visited[i] = true;
+                arr[depth] = i+1;
+                dfs(N, M, depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
