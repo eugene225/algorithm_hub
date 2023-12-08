5f680ab329411b61ae4012031feadd3eb61a563d
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-08 20:27:10 +0900

    [Silver I] Title: 금공강 사수, Time: 180 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Silver/27375. 금공강 사수/금공강 사수.java
+++ 백준/Silver/27375. 금공강 사수/금공강 사수.java
@@ -19,7 +19,7 @@ public class Main {
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int n, k;
     static List<Lesson> lessons = new ArrayList<>();
-    static boolean[][] visited;
+    static boolean[][] visited;  // visited[요일][시간]
     static int ans = 0;
 
     public static void main(String[] args) throws IOException {
@@ -35,7 +35,7 @@ public class Main {
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
 
-            if (w != 5) {
+            if (w != 5) {  // 금요일 수업 배열에 넣지 않음
                 lessons.add(new Lesson(w, s, e, e - s + 1));
             }
         }
@@ -53,22 +53,22 @@ public class Main {
         }
         for(int i=start; i<lessons.size(); i++) {
             if(isPossible(lessons.get(i))) {
-                changeStatus(lessons.get(i), true);
+                changeStatus(lessons.get(i), true);  // 시간표에 배정
 
                 solution(K, cnt+lessons.get(i).credit, i+1, lessons);
 
-                changeStatus(lessons.get(i), false);
+                changeStatus(lessons.get(i), false);  // 시간표에서 삭제
             }
         }
     }
 
-    private static void changeStatus(Lesson lesson, boolean status) {
+    private static void changeStatus(Lesson lesson, boolean status) {  // 시간표에 수업을 배정(true) 및 삭제(false)
         for(int i=lesson.start; i<= lesson.end; i++) {
             visited[lesson.day][i] = status;
         }
     }
 
-    private static boolean isPossible(Lesson lesson) {
+    private static boolean isPossible(Lesson lesson) {  // 해당 수업이 시간표에 들어갈 수 있는 지 확인
         for(int i=lesson.start; i<= lesson.end; i++) {
             if(visited[lesson.day][i]) return false;
         }
