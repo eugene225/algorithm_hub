52bd27e6a9f11bf398f0fc45cef667a0cf65325f
eugene <81062608+eugene225@users.noreply.github.com>
2024-01-09 00:23:46 +0900

    [Silver I] Title: 정수 삼각형, Time: 292 ms, Memory: 26152 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -3,20 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main
-{
-	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int N;
-    private static int[][] dp;
-    private static int ans;
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        dp = new int[N][N];
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N][N];
 
         dp[0][0] = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        ans = dp[0][0];
+        int ans = dp[0][0];
         for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < i+1; j++) {
@@ -28,7 +25,7 @@ public class Main
                 } else {
                     dp[i][j] += Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
                 }
-                
+
                 ans = Math.max(ans, dp[i][j]);
             }
         }
