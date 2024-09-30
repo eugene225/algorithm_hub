d4c9435f2175f0a2c41082049522d2341cfe4a4e
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-30 20:53:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.06 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,64 +1,56 @@
 import java.util.*;
 
-public class Point {
-    int x;
-    int y;
-    
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-    
-    public int getX() {
-        return this.x;
-    }
-    
-    public int getY() {
-        return this.y;
-    }
-}
-
 class Solution {
     
-    int[] dx = {1, 0, -1, 0};
-    int[] dy = {0, 1, 0, -1};
-    int[][] visited;
+    static int dx[] = {0,0,-1,1};
+    static int dy[] = {1,-1,0,0};
+    static int answer;
+    static int visited[][];
     
     public int solution(int[][] maps) {
-        int answer = 0;
         
         visited = new int[maps.length][maps[0].length];
-        
         bfs(maps);
+        
         answer = visited[maps.length-1][maps[0].length-1];
         
-        if(answer == 0) answer = -1;
+        if(answer==0) answer = -1;
         
         return answer;
     }
     
-    public void bfs(int[][] maps) {
-        visited[0][0] = 1;
+    public void bfs(int maps[][]) {
+        int x = 0;
+        int y = 0;
+        visited[x][y] = 1;
         
         Queue<Point> q = new LinkedList<>();
-        q.add(new Point(0, 0));
+        q.add(new Point(x,y));
         
         while(!q.isEmpty()) {
-            Point now = q.poll();
-            int cx = now.getX();
-            int cy = now.getY();
+            Point now = q.remove();
             
-            for(int i=0; i<4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
+            for(int i=0;i<4;i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
                 
-                if(nx < 0 || nx > maps.length-1 || ny < 0 || ny > maps[0].length-1) continue;
+                if(nx<0 || nx>maps.length-1 || ny<0 || ny>maps[0].length-1)
+                    continue;
                 
-                if(visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    visited[nx][ny] = visited[cx][cy] + 1;
+                if(visited[nx][ny]==0 && maps[nx][ny]==1) {
+                    visited[nx][ny] = visited[now.x][now.y] + 1;
                     q.add(new Point(nx, ny));
                 }
             }
         }
+    }
+}
+
+class Point{
+    int x,y;
+    
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
