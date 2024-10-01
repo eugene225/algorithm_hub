5cd7352fada896d2d7d1bcce4395fdf5c2d64cfd
eugene <81062608+eugene225@users.noreply.github.com>
2024-10-01 14:15:19 +0900

    [level 3] Title: 네트워크, Time: 0.99 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 67.6 MB, 시간: 0.18 ms
+메모리: 77.8 MB, 시간: 0.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 19:32:45
+2024년 10월 01일 14:15:17
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,32 +1,54 @@
-import java.io.*;
+import java.util.*;
 
 class Solution {
-    private boolean[] visited;
+    
+    private static ArrayList<ArrayList<Integer>> graph;
+    private static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        
         visited = new boolean[n];
+        graph = new ArrayList<ArrayList<Integer>>();
+        
+        for(int i=0;i<n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
         
         for(int i=0; i<n; i++) {
-            visited[i] = false;
+            for(int j=0; j<n; j++) {
+                if(computers[i][j] == 1) {
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }
+            }
         }
         
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
+                bfs(i);
                 answer++;
-                dfs(i, visited, computers);
             }
         }
         
         return answer;
     }
     
-    public void dfs(int num, boolean[] visited, int[][] computers) {
-        visited[num] = true;
+    private static void bfs(int idx) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(graph.get(idx).get(0));
+        visited[idx] = true;
         
-        for(int i=0; i<computers.length; i++) {
-            if(!visited[i] && computers[num][i]==1) {
-                dfs(i, visited, computers);
+        int ans = 0;
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            
+            for(int i=0; i<graph.get(now).size(); i++) {
+                int child = graph.get(now).get(i);
+                if(!visited[child]) {
+                    visited[child] = true;
+                    q.add(child);
+                }
             }
         }
     }
