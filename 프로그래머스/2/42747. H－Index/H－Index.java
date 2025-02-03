b45fd58a1bde9e1e5e74ba31d5cd1df15bcb9085
eugene <81062608+eugene225@users.noreply.github.com>
2025-02-04 00:23:02 +0900

    [level 2] Title: H-Index, Time: 1.00 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,35 +1,19 @@
 import java.util.*;
 
 class Solution {
-    
-    private static Map<Integer, Integer> map;
-    
     public int solution(int[] citations) {
         int answer = 0;
         
-        map = new HashMap<Integer, Integer>();
-        for(int i=1; i<=citations.length; i++) {
-            addToMap(i, citations[i-1]);
-            //System.out.println(map);
-        }
+        Arrays.sort(citations);
         
-        //System.out.println(map);
-        Iterator<Integer> iter = map.keySet().iterator();
-        while(iter.hasNext()) {
-            int key = iter.next();
-            int value = map.get(key);
-            
-            //System.out.println(value + " " + answer);
-            if(key <= value && value != answer) {
-                answer = Math.max(key, answer);
+        int cnt = 0;
+        for(int i=0; i<citations.length; i++) {
+            if(citations.length-i < cnt) break;
+            while(citations[i] >= cnt && citations.length-i >= cnt) {
+                answer = cnt++;
+                //System.out.println("answer : " + answer + ", cnt : " + cnt + ", i : " + i);
             }
         }
         return answer;
-    }
-    
-    private void addToMap(int idx, int cnt) {
-        for(int i=1; i<=cnt; i++) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
     }
 }
