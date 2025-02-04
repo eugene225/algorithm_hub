e0889962f3519f981175ed1f2684a5ce0098968e
eugene <81062608+eugene225@users.noreply.github.com>
2025-02-05 00:59:36 +0900

    [level 2] Title: 타겟 넘버, Time: 6.76 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,20 +3,21 @@ class Solution {
     private static int ans;
     
     public int solution(int[] numbers, int target) {
+        int answer = 0;
+        
         ans = 0;
-        dfs(numbers, 0, 0, target);
+        dfs(0, 0, numbers, target);
         
         return ans;
     }
     
-    private static void dfs(int[] numbers, int idx, int tar, int target) {
-        if(idx == numbers.length && tar == target) {
-            ans = ans + 1;
+    private static void dfs(int now, int cnt, int[] numbers, int target) {
+        if(cnt == numbers.length) {
+            if(now == target) ans += 1;
             return;
         }
-        if(idx == numbers.length) return;
         
-        dfs(numbers, idx+1, tar + numbers[idx], target);
-        dfs(numbers, idx+1, tar - numbers[idx], target);
+        dfs(now+numbers[cnt], cnt+1, numbers, target);
+        dfs(now-numbers[cnt], cnt+1, numbers, target);
     }
 }
