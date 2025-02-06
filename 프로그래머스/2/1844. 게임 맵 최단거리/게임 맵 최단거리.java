08aa638ae81650bb9482ef752686ccbcce0cf04e
eugene <81062608+eugene225@users.noreply.github.com>
2025-02-06 16:03:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.43 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,55 +2,68 @@ import java.util.*;
 
 class Solution {
     
-    static int dx[] = {0,0,-1,1};
-    static int dy[] = {1,-1,0,0};
-    static int answer;
-    static int visited[][];
+    private static int N, M;
+    private static int[][] visited;
+    private static int ans;
     
     public int solution(int[][] maps) {
+        int answer = 0;
         
-        visited = new int[maps.length][maps[0].length];
+        N = maps.length;
+        M = maps[0].length;
+        visited = new int[N][M];
+        
         bfs(maps);
         
-        answer = visited[maps.length-1][maps[0].length-1];
+        ans = visited[N-1][M-1];
+        if(visited[N-1][M-1] == 0) ans = -1;
         
-        if(answer==0) answer = -1;
-        
-        return answer;
+        return ans;
     }
     
-    public void bfs(int maps[][]) {
-        int x = 0;
-        int y = 0;
-        visited[x][y] = 1;
-        
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(x,y));
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
+    private static void bfs(int[][] maps) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
+        visited[0][0] = 1;
         
         while(!q.isEmpty()) {
-            Point now = q.remove();
+            int[] cur = q.poll();
             
-            for(int i=0;i<4;i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
+            for(int i=0; i<4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
                 
-                if(nx<0 || nx>maps.length-1 || ny<0 || ny>maps[0].length-1)
-                    continue;
+                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
                 
-                if(visited[nx][ny]==0 && maps[nx][ny]==1) {
-                    visited[nx][ny] = visited[now.x][now.y] + 1;
-                    q.add(new Point(nx, ny));
+                if(visited[nx][ny] == 0 && maps[nx][ny] == 1) {
+                    visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
+                    q.add(new int[]{nx, ny});
                 }
             }
         }
     }
-}
-
-class Point{
-    int x,y;
     
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+//     private static void dfs(int[] cur, int cnt, int[][] maps) {
+//         if(cur[0] == N-1 && cur[1] == M-1) {
+//             ans = Math.min(ans, cnt);
+//             //System.out.println("ans : " + ans + ", cnt : " + cnt);
+//             return;
+//         }
+        
+//         for(int i=0; i<4; i++) {
+//             int nx = cur[0] + dx[i];
+//             int ny = cur[1] + dy[i];
+            
+//             if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            
+//             if(!visited[nx][ny] && maps[nx][ny] == 1) {
+//                 visited[nx][ny] = true;
+//                 dfs(new int[]{nx, ny}, cnt+1, maps);
+//                 visited[nx][ny] = false;
+//             }
+//         }
+//     }
 }
