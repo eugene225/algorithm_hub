514d3d4a85878cf2b77e6493c0580a3723e15e53
eugene <81062608+eugene225@users.noreply.github.com>
2025-02-06 14:40:49 +0900

    [level 3] Title: 네트워크, Time: 0.40 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,31 +2,15 @@ import java.util.*;
 
 class Solution {
     
-    private static ArrayList<ArrayList<Integer>> graph;
     private static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
         visited = new boolean[n];
-        graph = new ArrayList<ArrayList<Integer>>();
-        
-        for(int i=0;i<n; i++) {
-            graph.add(new ArrayList<Integer>());
-        }
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(computers[i][j] == 1) {
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
-                }
-            }
-        }
-        
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
-                bfs(i);
+                bfs(i, computers, n);
                 answer++;
             }
         }
@@ -34,20 +18,17 @@ class Solution {
         return answer;
     }
     
-    private static void bfs(int idx) {
+    private static void bfs(int idx, int[][] computers, int n) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(graph.get(idx).get(0));
-        visited[idx] = true;
+        q.offer(idx);
         
-        int ans = 0;
         while(!q.isEmpty()) {
             int now = q.poll();
             
-            for(int i=0; i<graph.get(now).size(); i++) {
-                int child = graph.get(now).get(i);
-                if(!visited[child]) {
-                    visited[child] = true;
-                    q.add(child);
+            for(int i=0; i<n; i++) {
+                if(!visited[i] && computers[now][i] == 1) {
+                    visited[i] = true;
+                    q.offer(i);
                 }
             }
         }
